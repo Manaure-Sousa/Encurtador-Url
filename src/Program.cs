91cb17e7b501c5631d533714ec8f64a7a0544188
Manaure Vasconcelos <manaurevasc97@gmail.com>
2026-05-08 13:47:40 -0300

    feat: add Entity Framework Core configuration

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,18 @@
+using EncurtadorUrl.src.Data;
+using EncurtadorUrl.src.Endpoints;
+using EncurtadorUrl.src.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+
+builder.Services.AddScoped<UrlShorteningService>();
+
 var app = builder.Build();
+
+app.MapShortenUrlEndpoint();
 
 app.UseHttpsRedirection();
 
