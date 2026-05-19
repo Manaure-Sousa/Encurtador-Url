42a13fdc2fd9453e48db220b0b2f279fa811095c
Manaure Vasconcelos <manaurevasc97@gmail.com>
2026-05-18 21:43:08 -0300

    chore: setup JWT authentication

--- src/Program.cs
+++ src/Program.cs
@@ -1,18 +1,43 @@
+using System.Text;
 using System.Threading.RateLimiting;
+using EncurtadorUrl.src.Auth;
 using EncurtadorUrl.src.Data;
 using EncurtadorUrl.src.Endpoints;
 using EncurtadorUrl.src.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 
+builder.Services.AddScoped<AuthService>();
+
 builder.Services.AddScoped<UrlShorteningService>();
 
-// Configurar CORS para aceitar requisições do frontend
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"] ?? throw new InvalidOperationException("JWT issuer not configured."),
+        ValidAudience = builder.Configuration["Jwt:Audience"] ?? throw new InvalidOperationException("JWT audience not configured."),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT key not configured.")))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
@@ -25,22 +50,27 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddRateLimiter(options =>
 {
-   options.RejectionStatusCode = StatusCodes.Status429TooManyRequests; 
-   options.AddFixedWindowLimiter("FixedWindowPolicy", opt =>
-   {
-       opt.Window = TimeSpan.FromSeconds(10);
-       opt.PermitLimit = 5;
-       opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-       opt.QueueLimit = 2;
-   });
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.AddFixedWindowLimiter("FixedWindowPolicy", opt =>
+    {
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.PermitLimit = 5;
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
 });
 
 var app = builder.Build();
 
-// Aplicar política de CORS
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.UseCors("AllowFrontend");
 
 app.MapShortenUrlEndpoint();
+
+app.MapAuthEndpoints();
 
 app.UseHttpsRedirection();
 
